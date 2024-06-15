069cc233291ee128c355681e8b5e30872f577c2d
douglas sales <douglas-8545@hotmail.com>
2024-06-15 17:58:30 -0300

    feature: added a new components in page

--- src/app/CreateAccount/page.tsx
+++ src/app/CreateAccount/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import ButtonEntrarAccount from "../components/CreateAccount/ButtonEntrarAccount";
+import CreateAccountButton from "../components/CreateAccount/CreateAccountButton";
+import FormCreateAccount from "../components/CreateAccount/FormCreateAccount";
 import { ButtonCreateAccount } from "../components/Login/ButtonCreateAccount";
 
-export default function CreateAccount () {
-    return (
+export default function CreateAccount() {
+  return (
     <div className="w-full h-screen flex flex-col items-center bg-whitePrimary">
-    <ButtonEntrarAccount/>   
+      <ButtonEntrarAccount />
+      <FormCreateAccount />
+      <CreateAccountButton />
     </div>
-    )
+  );
 }
