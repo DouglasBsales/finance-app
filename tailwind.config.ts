b5dcc295171ef0fc35d4875bde1ac6846de78c24
douglas sales <douglas-8545@hotmail.com>
2024-06-15 16:58:26 -0300

    feature: added new colors project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        whitePrimary: "#F5F5F7", 
+        bluePrimary: "#1877F2",
+        blackPrimary: "#3C3C3C",
+        blackOpacity: "#B8B8B8",
+      }
     },
   },
   plugins: [],
