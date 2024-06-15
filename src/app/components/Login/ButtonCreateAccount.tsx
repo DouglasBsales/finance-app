7bc40fdee847e2cb65ba4e9b8ea0c176c6a37e04
douglas sales <douglas-8545@hotmail.com>
2024-06-15 16:59:48 -0300

    feature: added a new components

---
+++ src/app/components/Login/ButtonCreateAccount.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export const ButtonCreateAccount = ()=> {
+    return (
+    <div className="w-full flex justify-end mt-[46px] ">
+        <div className="">
+           <button className="px-4 py-2 border-2 border-bluePrimary rounded-md">
+                <Link href="/CreateAccount" className="text-bluePrimary font-semibold">Criar conta</Link>
+           </button>
+        </div>
+    </div>
+)
+}
