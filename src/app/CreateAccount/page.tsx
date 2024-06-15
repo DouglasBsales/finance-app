23d00b142d94e77caec21fd3f464650699890236
douglas sales <douglas-8545@hotmail.com>
2024-06-15 16:59:59 -0300

    feature: added page CreateAccount

---
+++ src/app/CreateAccount/page.tsx
@@ -0,0 +1,10 @@
+import ButtonEntrarAccount from "../components/CreateAccount/ButtonEntrarAccount";
+import { ButtonCreateAccount } from "../components/Login/ButtonCreateAccount";
+
+export default function CreateAccount () {
+    return (
+    <div className="w-full h-screen flex flex-col items-center bg-whitePrimary">
+    <ButtonEntrarAccount/>   
+    </div>
+    )
+}
