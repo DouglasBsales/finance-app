aca0aaf70fd4188bcd9263a057190c836985f2f6
douglas sales <douglas-8545@hotmail.com>
2024-06-15 17:58:05 -0300

    feature: added a new component of page CreateAccount

--- src/app/components/Login/ButtonEntrar.tsx
+++ src/app/components/Login/ButtonEntrar.tsx
@@ -1,14 +1,15 @@
-
 const ButtonEntrar = () => {
   return (
     <div className="w-full flex justify-center pt-[36px]">
-    <div>
+      <div>
         <button className="bg-bluePrimary rounded-md outline-none">
-            <p className="px-[112px] py-[10px] font-smeibold text-[20px] text-white">Entrar</p>
+          <p className="px-[112px] py-[10px] font-semibold text-[20px] text-white">
+            Entrar
+          </p>
         </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default ButtonEntrar
+export default ButtonEntrar;
