f4cb84cc0db615463290e423e7d243752be6e556
douglas sales <douglas-8545@hotmail.com>
2024-06-15 16:59:32 -0300

    remove archives

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="h-screen flex justify-center items-center text-4xl">
-      <h1>Hello world</h1>
-    </div>
-  );
-}
