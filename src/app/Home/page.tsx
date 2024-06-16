145b9e5f015a713638a82f87dede34d68923b7b9
douglas sales <douglas-8545@hotmail.com>
2024-06-16 19:14:22 -0300

    feature: added useffect of getUser in banc of data

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useContext, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { HomeContext } from "@/Context/HomeContext";
+import { db } from "@/services/firebase";
+
 export default function Home() {
+  const { dataUser, setDataUsers} = useContext(HomeContext);
+
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setDataUsers(data.docs.map((doc:any) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, [setDataUsers, userCollectionRef]);
+
+
   return (
     <div className="w-full h-screen flex flex-col items-center bg-whitePrimary">
-      <p>Olá</p>
+      <div className="flex flex-col">
+        <p>Olá</p>
+        {dataUser.map((users: any) => (
+          <p key={users.id}>{users.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
