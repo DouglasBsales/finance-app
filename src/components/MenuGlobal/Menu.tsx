5c8716c73fc3561fb80ff340cf077df424310f14
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-06-18 18:41:33 +0000

    feature: added logics of menu

--- src/components/MenuGlobal/Menu.tsx
+++ src/components/MenuGlobal/Menu.tsx
@@ -1,11 +1,107 @@
-import React from 'react'
+"use client";
+
+import {
+  faClockRotateLeft,
+  faHouse,
+  faMagnifyingGlass,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 
 const Menu = () => {
-  return (
-    <div className="w-full h-[66px] bg-white fixed bottom-0">
-      
-    </div>
-  )
-}
+  const [isPage, setIsPage] = useState<string>("Home");
 
-export default Menu
+  const isPageHome = () => {
+    setIsPage("Home");
+  };
+
+  const isPageTransacao = () => {
+    setIsPage("pageTransacao");
+  };
+
+  const isPageSearchTransition = () => {
+    setIsPage("searchTransition");
+  };
+
+  const isPageConfigUser = () => {
+    setIsPage("pageConfigUser");
+  };
+
+  return (
+    <div className="w-full h-[70px] flex justify-center items-center bg-white fixed bottom-0">
+      <div className="flex w-[310px] items-center gap-[62px]">
+      <button onClick={isPageHome}>
+          <FontAwesomeIcon
+            icon={faHouse}
+            className={`${
+              isPage === "Home"
+                ? "text-bluePrimary "
+                : "text-blackOpacity "
+            } text-4xl`}
+          />
+          <div
+            className={`border ${
+              isPage === "Home"
+                ? "border-bluePrimary"
+                : " border-white"
+            } mt-[8px]`}
+          ></div>
+        </button>
+        <button onClick={isPageTransacao}>
+          <FontAwesomeIcon
+            icon={faClockRotateLeft}
+            className={`${
+              isPage === "pageTransacao"
+                ? "text-bluePrimary "
+                : "text-blackOpacity "
+            } text-4xl`}
+          />
+          <div
+            className={`border ${
+              isPage === "pageTransacao"
+                ? "border-bluePrimary"
+                : " border-white"
+            } mt-[8px]`}
+          ></div>
+        </button>
+        <button onClick={ isPageSearchTransition}>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={`${
+              isPage === "searchTransition"
+                ? "text-bluePrimary "
+                : "text-blackOpacity "
+            } text-4xl`}
+          />
+          <div
+            className={`border ${
+              isPage === "searchTransition"
+                ? "border-bluePrimary"
+                : " border-white"
+            } mt-[8px]`}
+          ></div>
+        </button>
+        <button onClick={isPageConfigUser}>
+          <FontAwesomeIcon
+            icon={faUser}
+            className={`${
+              isPage === "pageConfigUser"
+                ? "text-bluePrimary "
+                : "text-blackOpacity "
+            } text-4xl`}
+          />
+          <div
+            className={`border ${
+              isPage === "pageConfigUser"
+                ? "border-bluePrimary"
+                : " border-white"
+            } mt-[8px]`}
+          ></div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
