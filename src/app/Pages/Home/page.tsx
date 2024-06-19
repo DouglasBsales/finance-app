3d8443cdbf97c814aef27a77299c54534494b145
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-06-19 14:06:06 +0000

    Reafctor: changed class and add elements

---
+++ src/app/Pages/Home/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Header } from "@/components/HomePage/Header";
+import { Planos } from "@/components/HomePage/Planos";
+
+export default function Home() {
+  return (
+    <div className="w-full flex flex-col items-center bg-whitePrimary pb-[100px]">
+      <Header />
+      <div className="w-[390px] ">
+      <Planos />
+      </div>
+    </div>
+  );
+}
