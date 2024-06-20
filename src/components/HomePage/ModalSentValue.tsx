54db345da09a972a3ff4814e38d12ed5af9313c2
douglas sales <douglas-8545@hotmail.com>
2024-06-19 22:57:02 -0300

    Feature: added function of change valueWallet

--- src/components/HomePage/ModalSentValue.tsx
+++ src/components/HomePage/ModalSentValue.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { updateDoc } from "firebase/firestore";
+import { HomeContext } from "@/Context/HomeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleArrowUp,
   faMoneyCheckDollar,
 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type ModalSentValueProps = {
   setOpenModalSentValue: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ModalSentValue: React.FC<ModalSentValueProps> = ({ setOpenModalSentValue }) => {
+const ModalSentValue: React.FC<ModalSentValueProps> = ({
+  setOpenModalSentValue,
+}) => {
+  const { valueWallet, idWalletAtt } = useContext(HomeContext);
 
-    const closedModal = ()=> {
-        setOpenModalSentValue(false)
-    }
+  const numberWallet = valueWallet.reduce(
+    (acc: number, wallet: any) => acc + parseFloat(wallet.valueWallet),
+    0
+  );
 
-    // criar funcao para adicionar os dados de valor adicionaos
+  const [valueSentWallet, setValueSentWallet] = useState<string>("");
+
+  const changeSentValueWallet = async () => {
+    const changeValueOfNumber = parseFloat(valueSentWallet);
+
+    const newValueWallet = numberWallet + changeValueOfNumber;
+
+    await updateDoc(idWalletAtt, { valueWallet: newValueWallet });
+    setOpenModalSentValue(false);
+  };
+
+  const closedModal = () => {
+    setOpenModalSentValue(false);
+  };
+
+  // criar funcao para adicionar os dados de valor adicionaos
 
   return (
     <div className="w-full fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
@@ -41,17 +62,21 @@ const ModalSentValue: React.FC<ModalSentValueProps> = ({ setOpenModalSentValue }
                 type="text"
                 className="rounded-md outline-none pl-[9px] text-blackPrimary"
                 placeholder="ex: 100"
+                onChange={(e) => setValueSentWallet(e.target.value)}
               />
             </div>
           </div>
         </div>
         <div className="flex gap-[11px]">
-            <button className="border-2 rounded-md" onClick={closedModal}>
-                <p className="py-[8px] px-4 text-blackOpacity">Cancelar</p>
-            </button>
-            <button className="bg-bluePrimary rounded-md">
-                <p className="py-[8px] px-4 text-white">Confirmar</p>
-            </button>
+          <button className="border-2 rounded-md" onClick={closedModal}>
+            <p className="py-[8px] px-4 text-blackOpacity">Cancelar</p>
+          </button>
+          <button
+            className="bg-bluePrimary rounded-md"
+            onClick={changeSentValueWallet}
+          >
+            <p className="py-[8px] px-4 text-white">Confirmar</p>
+          </button>
         </div>
       </div>
     </div>
