7c1a32482d5695900637f46c5717af9d6ceda152
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-06-20 20:14:11 +0000

    Feature: created a new component

---
+++ src/components/HomePage/LastTransacoes.tsx
@@ -0,0 +1,14 @@
+const LastTransacoes = () => {
+  return (
+    <div className="pl-[28px] pt-[25px]">
+      <div className="flex justify-between items-center">
+        <p className="text-blackPrimary text-xl font-medium">Ultimas transações</p>
+        <button className="text-bluePrimary font-medium pr-[28px]">
+         Ver todas
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LastTransacoes;
