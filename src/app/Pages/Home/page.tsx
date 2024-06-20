7018826c8134d3658461b8b5f1055f54e4088389
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-06-20 20:13:44 +0000

    Refactor: added a new component

--- src/app/Pages/Home/page.tsx
+++ src/app/Pages/Home/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { Custos } from "@/components/HomePage/Custos";
 import { Header } from "@/components/HomePage/Header";
+import LastTransacoes from "@/components/HomePage/LastTransacoes";
 import { Planos } from "@/components/HomePage/Planos";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center bg-whitePrimary pb-[100px]">
       <Header />
-      <div className="w-[360px] ">
+      <div className="w-[360px]">
       <Planos />
+      <Custos/>
+      <LastTransacoes/>
       </div>
     </div>
   );
