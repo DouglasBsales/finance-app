683c11eee3f1e7be70e74eb43e191f1df8147bef
douglas sales <douglas-8545@hotmail.com>
2024-06-22 12:30:20 -0300

    Refactor: changed logic

--- src/components/HomePage/Planos.tsx
+++ src/components/HomePage/Planos.tsx
@@ -1,51 +1,36 @@
+import { HomeContext } from "@/Context/HomeContext";
 import { faHouse, faUmbrellaBeach } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useContext } from "react";
+import CardsPlans from "./CardsPlans";
 
 export const Planos = () => {
+  const { plansData } = useContext(HomeContext);
+
   return (
     <div className="pl-[28px] pt-[25px]">
       <div className="flex justify-between items-center">
         <p className="text-blackPrimary text-xl font-medium">Seus planos</p>
-        <Link href="/Pages/NewPlan" className="text-bluePrimary font-medium pr-[28px]">
+        <Link
+          href="/Pages/NewPlan"
+          className="text-bluePrimary font-medium pr-[28px]"
+        >
           Novo plano
         </Link>
       </div>
       <div className="flex gap-3 pt-5">
-        <div className=" w-[130px] rounded-md bg-white pl-3 pb-3">
-          <div className="pt-4">
-            <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
-              <FontAwesomeIcon
-                icon={faHouse}
-                className="text-bluePrimary text-xl"
-              />
-            </div>
+        {plansData.length > 0 ? (
+          <div>
+            <p className="text-blackOpacity">Você ainda nao possui planos a serem exibidos</p>
           </div>
-          <div className="pt-4">
-            <p className="text-blackPrimary font-semibold">House</p>
-            <div className="flex gap-1">
-              <p className="text-xs font-medium text-blackPrimary">R$ 31.4k</p>
-              <p className="text-xs font-medium text-blackOpacity">/R$ 200k</p>
+        ) : (
+          plansData.map((plan: any) => (
+            <div key={plan.id}>
+              <CardsPlans />
             </div>
-          </div>
-        </div>
-        <div className=" w-[130px] rounded-md bg-white pl-3">
-          <div className="pt-4">
-            <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
-              <FontAwesomeIcon
-                icon={faUmbrellaBeach}
-                className="text-bluePrimary text-xl"
-              />
-            </div>
-          </div>
-          <div className="pt-4">
-            <p className="text-blackPrimary font-semibold">House</p>
-            <div className="flex gap-1">
-              <p className="text-xs font-medium text-blackPrimary">R$ 31.4k</p>
-              <p className="text-xs font-medium text-blackOpacity">/R$ 200k</p>
-            </div>
-          </div>
-        </div>
+          ))
+        )}
       </div>
     </div>
   );
