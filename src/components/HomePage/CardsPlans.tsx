a8247367793299173b391652b7e4dcaa7137e230
douglas sales <douglas-8545@hotmail.com>
2024-06-23 16:30:14 -0300

    Feature: added props and functions to component dinamic

--- src/components/HomePage/CardsPlans.tsx
+++ src/components/HomePage/CardsPlans.tsx
@@ -1,26 +1,53 @@
+import { FunctionComponent, useContext } from "react";
+
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { HomeContext } from "@/Context/HomeContext";
 
-const CardsPlans = () => {
+type CardPlansProps = {
+  nameOfPlan: string;
+  valueOfPlan: number;
+  planId: string;
+};
+
+const CardsPlans: FunctionComponent<CardPlansProps> = ({
+  nameOfPlan,
+  valueOfPlan,
+  planId,
+}) => {
+  const { plansData, setPlanSelectIfId } = useContext(HomeContext);
+
+  const selectPlan = (id: string): void => {
+    const planSelected = plansData.map((plan: any) =>
+      plan.planos.find((plano: any) => plano.id === id)
+    );
+    setPlanSelectIfId(planSelected);
+  };
+
   return (
-    <div className=" w-[130px] rounded-md bg-white pl-3 pb-3">
-      <div className="pt-4">
-        <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
-          <FontAwesomeIcon
-            icon={faHouse}
-            className="text-bluePrimary text-xl"
-          />
+    <div className=" rounded-md bg-white px-3 pb-3">
+      <Link href="/Pages/Plan" onClick={() => selectPlan(planId)}>
+        <div className="pt-4">
+          <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
+            <FontAwesomeIcon
+              icon={faHouse}
+              className="text-bluePrimary text-xl"
+            />
+          </div>
         </div>
-      </div>
-      <div className="pt-4">
-        <p className="text-blackPrimary font-semibold">House</p>
-        <div className="flex gap-1">
-          <p className="text-xs font-medium text-blackPrimary">R$ 31.4k</p>
-          <p className="text-xs font-medium text-blackOpacity">/R$ 200k</p>
+        <div className="pt-4">
+          <p className="text-blackPrimary font-semibold">{nameOfPlan}</p>
+          <div className="flex gap-1">
+            <p className="text-xs font-medium text-blackPrimary">R$ 0,00</p>
+            <p className="text-xs font-medium text-blackOpacity">
+              /R$ {valueOfPlan}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
 
-export default CardsPlans
+export default CardsPlans;
