cf4ac19d1618a25e1cdca7e0202460f2aca0c5f3
douglas sales <douglas-8545@hotmail.com>
2024-06-24 13:06:29 -0300

    Refactor: changed class

--- src/app/Pages/Plan/page.tsx
+++ src/app/Pages/Plan/page.tsx
@@ -5,9 +5,11 @@ import HeaderPlan from "@/components/PlanPage/HeaderPlan";
 
 export default function Plan() {
   return (
-    <div className="w-full h-screen flex flex-col items-center bg-bluePrimary pb-[100px]">
-      <HeaderPlan />
-      <div className="w-[390px] px-7 pb-[40px]">
+    <div className="w-full h-screen flex flex-col items-center bg-bluePrimary">
+      <div className="w-full">
+        <HeaderPlan />
+      </div>
+      <div className="w-[390px] px-7 pb-[100px]">
         <div className="pt-12">
           <p className="text-white text-3xl font-medium">Ultimas transações</p>
           <CardsTransacoesPlan />
