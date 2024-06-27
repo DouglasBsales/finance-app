a9761fe3b57681c4c4457860c8140efce7f4816c
douglas sales <douglas-8545@hotmail.com>
2024-06-26 22:55:53 -0300

    Feature: adde a new component

---
+++ src/components/PlanPage/ModalExitValueWallet.tsx
@@ -0,0 +1,80 @@
+import { HomeContext } from "@/Context/HomeContext";
+import {
+  faCircleArrowUp,
+  faMoneyCheckDollar,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { doc, getDocs, updateDoc } from "firebase/firestore";
+import { useContext, useState } from "react";
+import { FunctionComponent } from "react";
+
+type ModalTypeProps = {
+  setShowModalExitValue: any;
+};
+
+const ModalExitValueWallet: FunctionComponent<ModalTypeProps> = ({ setShowModalExitValue }) => {
+  const { collectionRefPlan } = useContext(HomeContext);
+
+  const updateValueWalletPlan = async () => {
+    const docsPlan = await getDocs(collectionRefPlan);
+    const docPlanId = docsPlan.docs[0].id;
+    const refDocPlan = doc(collectionRefPlan, docPlanId);
+    await updateDoc(refDocPlan, {valueWallet: Number(valueExitWallet),});
+    setShowModalExitValue(false);
+  };
+
+  const [valueExitWallet, setValueExitWallet] = useState<string>();
+
+  return (
+    <div>
+      <div className="w-full fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="w-[263px] bg-whitePrimary px-4 rounded-[20px] pb-4">
+          <div className="pt-4">
+            <div className="flex items-center gap-3">
+              <FontAwesomeIcon
+                icon={faCircleArrowUp}
+                className="text-5xl text-bluePrimary"
+              />
+              <p className="text-blackPrimary text-xl font-medium">Entrada</p>
+            </div>
+            <div className="pt-4">
+              <p className="text-blackPrimary text-xl">Valor: </p>
+            </div>
+            <div className="w-[223px] pt-1 pb-[54px]">
+              <div className="flex items-center bg-white rounded-md pl-[9px]">
+                <FontAwesomeIcon
+                  icon={faMoneyCheckDollar}
+                  className="text-blackOpacity text-sm"
+                />
+                <input
+                  type="text"
+                  className="h-[40px] rounded-md outline-none pl-[9px] text-blackPrimary"
+                  placeholder="ex: 100"
+                  onChange={(e) => setValueExitWallet(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-[11px]">
+            <button
+              className="border-2 rounded-md"
+              onClick={() => setShowModalExitValue(false)}
+            >
+              <p className="py-[8px] px-4 text-blackOpacity">Cancelar</p>
+            </button>
+            <button className="bg-bluePrimary rounded-md">
+              <p
+                className="py-[8px] px-4 text-white"
+                onClick={updateValueWalletPlan}
+              >
+                Confirmar
+              </p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalExitValueWallet;
