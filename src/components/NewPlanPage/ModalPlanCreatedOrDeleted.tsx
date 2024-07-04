352b3fdc74577b9d768dbe9019fcb25508fa6ca6
douglas sales <douglas-8545@hotmail.com>
2024-07-03 22:33:16 -0300

    Refactor: changed name of component and add new conditional

---
+++ src/components/NewPlanPage/ModalPlanCreatedOrDeleted.tsx
@@ -0,0 +1,39 @@
+import { HomeContext } from "@/Context/HomeContext";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import { useContext } from "react";
+
+export const ModalPlanCreatedOrDeleted = () => {
+  const { optionPlan } = useContext(HomeContext);
+
+  return (
+    <div className="w-full fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="w-[263px] flex flex-col items-center bg-white rounded-md pb-5">
+        <div className="pt-[10px]">
+          <div className="w-[59px] h-[59px] flex items-center justify-center bg-bluePrimary rounded-full">
+            <FontAwesomeIcon
+              icon={faCircleCheck}
+              className="text-white text-[47px]"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center pt-[5px]">
+          <p className="font-bold text-bluePrimary">
+            {optionPlan === "created"
+              ? "Plano criado com sucesso"
+              : "Plano excluido com sucesso"}
+          </p>
+          <div className="flex items-center pt-3 gap-1">
+            {optionPlan === "created" ? <Image src="/loading2.gif" alt="loading" width={35} height={35} /> : ""}
+            <p className="text-sm text-blackOpacity text-center">
+              {optionPlan === "created"
+                ? "Carregando novo plano"
+                : "Você será redirecionado para a página Home"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
