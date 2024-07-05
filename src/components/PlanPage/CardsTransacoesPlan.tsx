d34dae8a835e14dfb8c955b861602e4b798580da
douglas sales <douglas-8545@hotmail.com>
2024-07-04 22:48:08 -0300

    Refator: changed component whit logics a render

--- src/components/PlanPage/CardsTransacoesPlan.tsx
+++ src/components/PlanPage/CardsTransacoesPlan.tsx
@@ -1,28 +1,48 @@
-import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { HomeContext } from "@/Context/HomeContext";
+import {
+  faCircleArrowDown,
+  faCircleChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FunctionComponent, useContext } from "react";
 
-const CardsTransacoesPlan = () => {
+type CardsProps = {
+  infoCards: Object;
+};
+
+const CardsTransacoesPlan: FunctionComponent<CardsProps> = ({infoCards}) => {
+  const { typeTransations } = useContext(HomeContext);
+
   return (
     <div className="pt-7">
-      <div className="w-[337px] flex items-center px-4 bg-white rounded-md">
-        <div className="flex justify-between pt-[22px] pb-3">
-          <div className="w-[42px] h-[42px] flex justfiy-center items-center bg-whitePrimary rounded-full">
-            <FontAwesomeIcon
-              icon={faCircleChevronDown}
-              className="text-[#46E068] text-[38px]"
-            />
-          </div>
-          <div className="pl-3">
-            <p className="font-medium text-blackPrimary ">
-              Entrada de dinheiro carteira
-            </p>
-            <p className="text-sm text-blackOpacity">Plano: House</p>
-          </div>
-          <div>
-            <p className="text-sm text-blackOpacity relative top-[3px]">
-              15/05/2024
-            </p>
-            <p className="text-[#46E068]"> + 100,00</p>
+      <div className="w-[337px] flex items-center pl-4 bg-white rounded-md">
+        <div className="w-full flex justify-between items-center py-3">
+          <div className="flex justfiy-center items-center">
+            {typeTransations === "walletHomeSent" ? (
+              <FontAwesomeIcon
+                icon={faCircleChevronDown}
+                className="text-[#46E068] text-[38px]"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faCircleArrowDown}
+                className="text-[#E8270E] text-[38px]"
+              />
+            )}
+            <div className="w-[180px] flex flex-col pl-3">
+              <p className="text-blackOpacity text-sm font-light">15/06/2024</p>
+              <p className="font-medium text-blackPrimary ">
+                {typeTransations === "walletHomeSent"
+                  ? "Entrada de dinheiro carteira"
+                  : "Saída de dinheiro carteira"}
+              </p>
+              <p className="text-sm text-blackOpacity font-medium">
+                Plano: House
+              </p>
+            </div>
+            <div className="flex items-center pl-5">
+              <p className="text-[#46E068]">+ 100,00</p>
+            </div>
           </div>
         </div>
       </div>
