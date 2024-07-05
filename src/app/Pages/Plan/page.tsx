7d4d8e2308cecc1b9dae6a10917826d0eaa98c6c
douglas sales <douglas-8545@hotmail.com>
2024-07-04 22:45:14 -0300

    Feature: added logic of render cardTransacoes

--- src/app/Pages/Plan/page.tsx
+++ src/app/Pages/Plan/page.tsx
@@ -2,18 +2,42 @@
 
 import CardsTransacoesPlan from "@/components/PlanPage/CardsTransacoesPlan";
 import HeaderPlan from "@/components/PlanPage/HeaderPlan";
+import { HomeContext } from "@/Context/HomeContext";
+import { useContext } from "react";
 
 export default function Plan() {
+  const { transations } = useContext(HomeContext);
+
+  const hasTransations: any =
+    Array.isArray(transations) &&
+    transations.length > 0 &&
+    Array.isArray(transations[0].planos) &&
+    transations[0].planos.length > 0;
+
   return (
     <div className="w-full h-screen flex flex-col items-center bg-bluePrimary">
       <div className="w-full">
         <HeaderPlan />
       </div>
       <div className="w-full px-7 pb-[100px] bg-bluePrimary">
-        <div className="flex flex-col items-center pt-12 px-7">
+        <p className="text-white text-3xl font-medium pt-12 ">
+          Ultimas transações
+        </p>
+        <div className="flex flex-col items-center">
           <div>
-            <p className="text-white text-3xl font-medium">Ultimas transações</p>
-            <CardsTransacoesPlan />
+            {hasTransations ? (
+              transations.map((cards: any) =>
+                cards.transacoes.slice(0, 5).map((transacao: any) => (
+                  <div key={transacao.id}>
+                    <CardsTransacoesPlan infoCards={{ ...transacao }} />
+                  </div>
+                ))
+              )
+            ) : (
+              <p className="text-whitePrimary">
+                Você ainda não possui transações neste plano
+              </p>
+            )}
           </div>
         </div>
       </div>
