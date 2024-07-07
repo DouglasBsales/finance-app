cdefd6b9e039d78f52ac886f6d29132a63757f10
douglas sales <douglas-8545@hotmail.com>
2024-07-06 22:01:19 -0300

    Refactor: Changed method of map array

--- src/app/Pages/Plan/page.tsx
+++ src/app/Pages/Plan/page.tsx
@@ -8,31 +8,38 @@ import { useContext } from "react";
 export default function Plan() {
   const { transations } = useContext(HomeContext);
 
-  const hasTransations: any =
-    Array.isArray(transations) &&
-    transations.length > 0 &&
-    Array.isArray(transations[0].planos) &&
-    transations[0].planos.length > 0;
+  const hasTransations = Array.isArray(transations) && transations.length > 0;
+
+  const allTransacoes = hasTransations
+    ? transations.flatMap((transaction: any) => transaction.transacoes)
+    : null;
 
   return (
     <div className="w-full h-screen flex flex-col items-center bg-bluePrimary">
       <div className="w-full">
         <HeaderPlan />
       </div>
-      <div className="w-full px-7 pb-[100px] bg-bluePrimary">
-        <div className="flex flex-col">
-        <p className="text-white text-3xl font-medium pt-12">
-          Ultimas transações
-        </p>
+      <div className="w-full flex flex-col items-center px-7 pb-[100px] bg-bluePrimary">
+        <div className="w-[390px] flex flex-col px-7 ">
+          <p className="text-white text-3xl font-medium pt-12">
+            Ultimas transações
+          </p>
           <div className="flex flex-col items-center">
-            {hasTransations ? (
-              transations.map((cards: any) =>
-                cards.transacoes.map((transacao: any) => (
-                  <div key={transacao.id}>
-                    <CardsTransacoesPlan infoCards={{ ...transacao }} />
-                  </div>
-                ))
-              )
+            {allTransacoes ? (
+              allTransacoes.map((transacao: any) => (
+                <div key={transacao.id}>
+                  <CardsTransacoesPlan
+                    infoCards={{
+                      date: transacao.data.date,
+                      icon: transacao.data.icon,
+                      id: transacao.id,
+                      name: transacao.data.name,
+                      value: transacao.data.value,
+                      sentValue: transacao.data.sentValue,
+                    }}
+                  />
+                </div>
+              ))
             ) : (
               <p className="text-whitePrimary">
                 Você ainda não possui transações neste plano
