e6fa725ce16b2fb90065fe0e34c90f902d20bc55
douglas sales <douglas-8545@hotmail.com>
2024-07-07 16:04:16 -0300

    Feature: added new logic of show transations

--- src/app/Pages/LastTransations/page.tsx
+++ src/app/Pages/LastTransations/page.tsx
@@ -2,6 +2,7 @@
 
 import CardsTransacoesPlan from "@/components/PlanPage/CardsTransacoesPlan";
 import { HomeContext } from "@/Context/HomeContext";
+import Image from "next/image";
 import { useContext } from "react";
 
 export default function LastTransations() {
@@ -14,27 +15,32 @@ export default function LastTransations() {
     ? transations.flatMap((transaction: any) => transaction.transacoes)
     : [];
 
-   
-
   return (
     <div className="w-full flex flex-col items-center bg-whitePrimary">
       <div className="w-[390px] overflow-x-hidden pb-[100px] px-7">
-        <p className=" text-blackPrimary font-medium text-xl pt-[66px]">Ulitmas movimentações</p>
-        {allTransacoes ?
-         allTransacoes.map((transacao: any) => (
-              <div key={transacao.id}>
-                <CardsTransacoesPlan 
-                 infoCards={{
+        <p className=" text-blackPrimary font-medium text-xl pt-[66px]">
+          Ulitmas movimentações
+        </p>
+        {allTransacoes.length > 0 ? (
+          allTransacoes.map((transacao: any) => (
+            <div key={transacao.id}>
+              <CardsTransacoesPlan
+                infoCards={{
                   date: transacao.data.date,
                   icon: transacao.data.icon,
                   id: transacao.id,
                   name: transacao.data.name,
                   value: transacao.data.value,
                   sentValue: transacao.data.sentValue,
-                }}/>
-              </div>
-            ))
-          : <p>Você ainda não possui transações</p>}
+                }}
+              />
+            </div>
+          ))
+        ) : (
+          <div>
+            <p>Você ainda não possui transações</p>
+          </div>
+        )}
       </div>
     </div>
   );
