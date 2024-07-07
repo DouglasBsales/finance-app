ae5a1256bb83d0cbd1a59a5df82cd6743ba7cae3
douglas sales <douglas-8545@hotmail.com>
2024-07-07 12:30:51 -0300

    Refactor: changed map

--- src/app/Pages/LastTransations/page.tsx
+++ src/app/Pages/LastTransations/page.tsx
@@ -5,7 +5,7 @@ import { HomeContext } from "@/Context/HomeContext";
 import { useContext } from "react";
 
 export default function LastTransations() {
-  const { transations, planSelected } = useContext(HomeContext);
+  const { transations } = useContext(HomeContext);
 
   const hasTransations = Array.isArray(transations) && transations.length > 0;
 
@@ -14,14 +14,14 @@ export default function LastTransations() {
     ? transations.flatMap((transaction: any) => transaction.transacoes)
     : [];
 
-    const filteredTransacoes = allTransacoes.filter((transacao: any) => transacao.data.name === planSelected.nameOfPlan)
+   
 
   return (
     <div className="w-full flex flex-col items-center bg-whitePrimary">
       <div className="w-[390px] overflow-x-hidden pb-[100px] px-7">
         <p className=" text-blackPrimary font-medium text-xl pt-[66px]">Ulitmas movimentações</p>
         {allTransacoes ?
-          filteredTransacoes.map((transacao: any) => (
+         allTransacoes.map((transacao: any) => (
               <div key={transacao.id}>
                 <CardsTransacoesPlan 
                  infoCards={{
