9208c9a99f1bcf654ccb0cc579c79df218235ac8
douglas sales <douglas-8545@hotmail.com>
2024-07-07 12:31:54 -0300

    Refactor: changed logic of render elements

--- src/components/PlanPage/CardsTransacoesPlan.tsx
+++ src/components/PlanPage/CardsTransacoesPlan.tsx
@@ -12,31 +12,24 @@ const CardsTransacoesPlan: FunctionComponent<CardsProps> = ({ infoCards }) => {
       {infoCards ? (
         <div className="w-[337px] flex items-center pl-4 bg-white rounded-md">
           <div className="w-full flex justify-between items-center py-3">
-            <div className="flex justfiy-center items-center">
-              <Image src={infoCards.icon} alt="" width={38} height={38} />
-              <div className="w-[180px] flex flex-col pl-3">
+            <Image src={infoCards.icon} alt="" width={38} height={38} />
+            <div className="">
+              <div className="w-[180px] flex flex-col">
                 <p className="text-blackOpacity text-sm font-light">
                   {infoCards.date}
                 </p>
-                <p className="font-medium text-blackPrimary ">
-                  {infoCards.name}
-                </p>
-                <p className="text-sm text-blackOpacity font-medium">
-                  {infoCards.plano}
-                </p>
+                <p className="font-medium text-blackPrimary ">{infoCards.name}</p>
+                {infoCards.plano && (
+                  <p className="text-sm text-blackOpacity font-medium">
+                    Plano: {infoCards.plano}
+                  </p>
+                )}
               </div>
-              <div className="flex items-center pl-5">
-                {" "}
-                {/* arrumar o padding left para que fique fixo independente do tamanho do texto */}
-                <p
-                  className={`${
-                    infoCards.sentValue ? "text-[#46E068]" : "text-[#FF0000]"
-                  }`}
-                >
-                  {infoCards.sentValue ? "+" : "-"}{" "}
-                  {formatarNumero(infoCards.value)}
-                </p>
-              </div>
+            </div>
+            <div className="flex items-center pr-3">
+              <p className={`${ infoCards.sentValue ? "text-[#46E068]" : "text-[#FF0000]"}`}>
+                {infoCards.sentValue ? "+" : "-"} {formatarNumero(infoCards.value)}
+              </p>
             </div>
           </div>
         </div>
