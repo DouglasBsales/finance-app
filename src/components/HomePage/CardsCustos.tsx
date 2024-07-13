2a5d6931ffa79989ed52084bc9b6d18ac41bb0a0
douglas sales <douglas-8545@hotmail.com>
2024-07-13 18:25:34 -0300

    Feature: added new function of select dinamic custos

--- src/components/HomePage/CardsCustos.tsx
+++ src/components/HomePage/CardsCustos.tsx
@@ -1,32 +1,51 @@
 import { faCreditCard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
 import { formatarNumero } from "../PlanPage/HeaderPlan";
+import Image from "next/image";
+import Link from "next/link";
+import { HomeContext } from "@/Context/HomeContext";
 
 type CustosProps = {
   infoCustos: any;
 };
 
 export const CardsCustos: FunctionComponent<CustosProps> = ({ infoCustos }) => {
+  const { custosData,  setIsPlansOrCustos } = useContext(HomeContext);
+
+  const selectCustos = (id: string): void => {
+
+    setIsPlansOrCustos("custos")
+
+    const findCusto = custosData[0].custos.find((custo: any) => custo.id === id);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("custosSelected", JSON.stringify(findCusto));
+    }
+  };
+
   return (
     <div>
       <div className=" w-[145px] rounded-md bg-white pl-3 pb-3">
-        <div className="pt-4">
-          <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
-            <FontAwesomeIcon
-              icon={faCreditCard}
-              className="text-[#DAA520] text-xl"
-            />
+        <Link href="/Pages/Custos" onClick={()=> selectCustos(infoCustos.id)}>
+          <div className="pt-4">
+            <div className="w-9 h-9 flex justify-center items-center rounded-full bg-whitePrimary">
+              <Image
+                src={infoCustos.categoryIcon}
+                alt="iconeCusto"
+                width={23}
+                height={23}
+              />
+            </div>
           </div>
-        </div>
-        <div className="pt-4">
-          <p className="text-blackPrimary font-semibold">{infoCustos.name}</p>
-          <div className="flex gap-1">
-            <p className="text-xs font-medium text-blackPrimary">
-              {formatarNumero(infoCustos.value)}
-            </p>
+          <div className="pt-4">
+            <p className="text-blackPrimary font-semibold">{infoCustos.name}</p>
+            <div className="flex gap-1">
+              <p className="text-xs font-medium text-blackPrimary">
+                {formatarNumero(infoCustos.value)}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
