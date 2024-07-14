cc6a93dbb0e7fa6b0e2d60093669931d5973160c
douglas sales <douglas-8545@hotmail.com>
2024-07-13 22:52:24 -0300

    Feature: added useEffect updateCusto in fireBase

--- src/components/CustosPage/HeaderCustos.tsx
+++ src/components/CustosPage/HeaderCustos.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,11 +10,21 @@ import { formatarNumero } from "../PlanPage/HeaderPlan";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { HomeContext } from "@/Context/HomeContext";
+import { ModalCustoAdded } from "./ModalCustoAdded";
+import { arrayRemove, getDoc, updateDoc } from "firebase/firestore";
+import { useQueryClient } from "react-query";
 
-export const HeaderCustos = () => {
+type HeaderCustosProps = {
+  valueAllCustos: number
+}
 
-  const {custoSelected, setCustoSelected} = useContext(HomeContext)
+export const HeaderCustos:FunctionComponent<HeaderCustosProps> = ({valueAllCustos}) => {
+
+  const queryClient = useQueryClient()
+
+  const {custoSelected, setCustoSelected, refDocCustos} = useContext(HomeContext)
   const [isModalOptions, setIsModalOptions] = useState<boolean>(false)
+  const [isModalCustoAdded, setIsModalCustoAdded] = useState<boolean>(false)
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -24,9 +34,54 @@ export const HeaderCustos = () => {
         setCustoSelected(parseData);
       }
     }
-  }, []);
+  }, [valueAllCustos]);
 
-  const parseValue:number = custoSelected ? parseFloat(custoSelected.valueCusto): 0;
+  useEffect(()=> {
+    const updateCustos = async () => {
+      if (!custoSelected || !refDocCustos) return;
+  
+        // Remove o custo selecionado do Firestore
+        await updateDoc(refDocCustos, {
+          custos: arrayRemove({
+            id: custoSelected.id,
+            name: custoSelected.name,
+            icon: custoSelected.icon,
+            categoryCusto: custoSelected.categoryCusto,
+            value: custoSelected.value,
+          }),
+        });
+  
+      // ADIÇÃO DE NOVO CUSTO
+      const docSnap:any = await getDoc(refDocCustos);
+      const existingCustos = docSnap.data()?.custos || [];
+
+      const custoAtt = {
+        id: custoSelected.id,
+        name: custoSelected.name,
+        icon: custoSelected.icon,
+        categoryCusto: custoSelected.categoryCusto,
+        value: valueAllCustos
+      }
+
+      const attCustos = [ custoAtt, ...existingCustos]
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
+      await updateDoc(refDocCustos, {custos: attCustos})
+
+      if(typeof window !== "undefined"){
+        localStorage.setItem("custosSelected", JSON.stringify(custoAtt))
+      }
+
+      queryClient.invalidateQueries("custosData")
+
+    }
+
+    updateCustos()
+
+  }, [valueAllCustos])
+  
+
+  const parseValue:number = custoSelected ? parseFloat(custoSelected.value): 0;
 
   return (
     <div className="w-full flex flex-col items-center bg-white rounded-b-[30px] pb-5 overflow-x-hidden">
@@ -43,7 +98,7 @@ export const HeaderCustos = () => {
               <div>
                 <div className="w-[92px] h-[92px] flex justify-center items-center bg-whitePrimary rounded-full">
                   <Image
-                    src={custoSelected.categoryIcon}
+                    src={custoSelected.icon}
                     alt="Imagem do plano"
                     width={50}
                     height={50}
@@ -52,7 +107,7 @@ export const HeaderCustos = () => {
               </div>
               <div className="flex flex-col justify-center">
                 <p className="text-[28px] text-blackPrimary font-medium">
-                  {custoSelected.nameCusto}
+                  {custoSelected.name}
                 </p>
                 <p className="text-blackPrimary">Categoria:</p>
                 <p className="text-blackOpacity relative">
@@ -74,17 +129,18 @@ export const HeaderCustos = () => {
             <div>
               <p className="font-medium text-blackPrimary">Valor atual</p>
               <p className="text-xl text-bluePrimary font-medium">
-                {formatarNumero(parseValue)}
+                R$ {formatarNumero(valueAllCustos)}
               </p>
             </div>
           </div>
           <div className="pt-6">
             <div className="flex gap-[37px]">
-              <button className="bg-bluePrimary rounded-[20px] px-3 py-[10px]">
+              <button className="bg-bluePrimary rounded-[20px] px-3 py-[10px]" onClick={()=>setIsModalCustoAdded(true)}>
                 <p className="text-white font-semibold">Adicionar custo</p>
               </button>
             </div>
           </div>
+          {isModalCustoAdded && <ModalCustoAdded setIsModalCustoAdded={setIsModalCustoAdded} valueAllCustos={valueAllCustos}/>}
         </div>
       ) : (
         ""
