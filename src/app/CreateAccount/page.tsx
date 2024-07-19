166e3cd8c212aee61d147435cb7597b23f7bba12
douglas sales <douglas-8545@hotmail.com>
2024-07-18 22:42:57 -0300

    Refactor: changed all components of page CreateAccount

--- src/app/CreateAccount/page.tsx
+++ src/app/CreateAccount/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ButtonEntrarAccount from "@/components/CreateAccount/ButtonEntrarAccount";
 import CreateAccountButton from "@/components/CreateAccount/CreateAccountButton";
 import FormCreateAccount from "@/components/CreateAccount/FormCreateAccount";
 
@@ -8,7 +7,6 @@ export default function CreateAccount() {
   return (
     <div className="w-full flex flex-col items-center bg-whitePrimary">
       <div className="w-[390px] px-[28px] pb-5">
-        <ButtonEntrarAccount />
         <FormCreateAccount />
         <CreateAccountButton />
       </div>
--- src/components/CreateAccount/CreateAccountButton.tsx
+++ src/components/CreateAccount/CreateAccountButton.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const CreateAccountButton = () => {
   return (
-    <div className="w-full flex justify-center pt-[36px]">
+    <div className="w-full flex flex-col items-center justify-center pt-5">
       <div>
         <button className="bg-bluePrimary rounded-md outline-none">
           <p className="w-[333px] py-[10px] font-semibold text-[20px] text-white ">
             Criar conta
           </p>
         </button>
+      </div>
+      <div className="flex gap-1 justify-center pt-4">
+        <p className="text-xs text-blackSecondary">Já possui uma conta?</p>
+        <Link href="/Login" className="text-xs text-bluePrimary">Entrar</Link>
       </div>
     </div>
   );
